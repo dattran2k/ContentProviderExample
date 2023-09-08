7cd67bb91d688ff243d3707a0946e48c14980744
dattran <dattran@vccorp.vn>
2023-09-08 11:19:15 +0700

    Initial commit

---
+++ app/src/main/java/com/dat/contentproviderexample/MyContentProvider.kt
@@ -0,0 +1,38 @@
+package com.dat.contentproviderexample
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class MyContentProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun query(
+        uri : Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+}
